ef3a2489cda87c3d3024b2a31be88909addae071
kietelmuis <omar.hopman9@gmail.com>
2026-03-10 20:08:42 +0100

    handle EMSG_PACKAGE_RESTRICTION_INFO

--- src/feats/misc.cpp
+++ src/feats/misc.cpp
@@ -4,40 +4,40 @@
 
 #include "../config.hpp"
 
+void Misc::recvMsg(CProtoBufMsgBase *msg) {
+  switch (msg->type) {
+  case EMSG_WALLET_INFO_UPDATE: {
+    const int32_t amount = g_config.fakeWalletBalance.get();
+    if (!amount) {
+      return;
+    }
 
-void Misc::recvMsg(CProtoBufMsgBase *msg)
-{
-	switch(msg->type)
-	{
-		case EMSG_WALLET_INFO_UPDATE:
-		{
-			const int32_t amount = g_config.fakeWalletBalance.get();
-			if (!amount)
-			{
-				return;
-			}
+    const auto body = msg->getBody<CMsgClientWalletInfoUpdate>();
+    body->set_has_wallet(true);
+    body->set_balance(amount);
+    body->set_balance64(amount);
 
-			const auto body = msg->getBody<CMsgClientWalletInfoUpdate>();
-			body->set_has_wallet(true);
-			body->set_balance(amount);
-			body->set_balance64(amount);
+    break;
+  }
+  case EMSG_PACKAGE_RESTRICTION_INFO: {
+    const auto body = msg->getBody<PackageRestrictionResponse>();
+    body->isRestricted = false;
 
-			break;
-		}
+    g_pLog->notify("Bypassing restriction for PackageID: %u\n",
+                   body->packageID);
+    break;
+  }
+  case EMSG_EMAIL_ADDRESS_INFO: {
+    const auto email = g_config.fakeEmail.get();
+    if (email.size() < 1) {
+      return;
+    }
 
-		case EMSG_EMAIL_ADDRESS_INFO:
-		{
-			const auto email = g_config.fakeEmail.get();
-			if (email.size() < 1)
-			{
-				return;
-			}
+    const auto body = msg->getBody<CMsgClientEmailAddrInfo>();
+    body->set_email_address(email);
+    body->set_email_is_validated(true);
 
-			const auto body = msg->getBody<CMsgClientEmailAddrInfo>();
-			body->set_email_address(email);
-			body->set_email_is_validated(true);
-
-			break;
-		}
-	}
+    break;
+  }
+  }
 }
